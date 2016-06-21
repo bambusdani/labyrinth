a85e1c35d5037c01f2ae3a4342e331529534c3ce
Rehan1 <app.rehan@yahoo.de>
2016-06-21 12:05:29 +0200

    rotate added to shape

--- src/Test/test.java
+++ src/Test/test.java
@@ -11,37 +11,10 @@ import java.io.File;
 public class test{
 
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame("no image");
-        ImageIcon yourImage = new ImageIcon("src/resources/arrows/downArrow.png");
-
-        JLabel imagelabel = new JLabel(rotate(yourImage,90));
-        frame.setVisible(true);
-        frame.add(imagelabel, BorderLayout.CENTER);
 
 
     }
 
-    public static ImageIcon rotate(ImageIcon imageIcon, double degrees){
-        //to Buffered Image
-        BufferedImage buImg = new BufferedImage(imageIcon.getIconWidth(), imageIcon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
-        buImg.getGraphics().drawImage(imageIcon.getImage(), 0,0, imageIcon.getImageObserver());
 
-        //drehen
-        AffineTransform affineTransform = AffineTransform.getRotateInstance(
-                Math.toRadians(degrees),
-                buImg.getWidth() / 2,
-                buImg.getHeight() / 2);
-        BufferedImage rotatedImage = new BufferedImage(buImg.getWidth(), buImg
-                .getHeight(), buImg.getType());
-        Graphics2D g = (Graphics2D) rotatedImage.getGraphics();
-        g.setTransform(affineTransform);
-        g.drawImage(buImg, 0, 0, null);
-
-        //to ImageIcon
-        ImageIcon imageI = new ImageIcon(rotatedImage);
-
-        return imageI;
-
-    }
 
 }
--- src/gameLogic/Shape.java
+++ src/gameLogic/Shape.java
@@ -2,6 +2,7 @@ package gameLogic;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public class Shape {
@@ -28,24 +29,26 @@ public class Shape {
 
 
     /** Rotate Image Function */
-    public BufferedImage rotate(BufferedImage image, double angle) {
-        double sin = Math.abs(Math.sin(angle)), cos = Math.abs(Math.cos(angle));
-        int w = image.getWidth(), h = image.getHeight();
-        int neww = (int)Math.floor(w*cos+h*sin), newh = (int) Math.floor(h * cos + w * sin);
-        GraphicsConfiguration gc = getDefaultConfiguration();
-        BufferedImage result = gc.createCompatibleImage(neww, newh, Transparency.TRANSLUCENT);
-        Graphics2D g = result.createGraphics();
-        g.translate((neww - w) / 2, (newh - h) / 2);
-        g.rotate(angle, w / 2, h / 2);
-        g.drawRenderedImage(image, null);
-        g.dispose();
-        return result;
-    }
+    public static ImageIcon rotateImage(ImageIcon imageIcon, double degrees){
+        //to Buffered Image
+        BufferedImage buImg = new BufferedImage(imageIcon.getIconWidth(), imageIcon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+        buImg.getGraphics().drawImage(imageIcon.getImage(), 0,0, imageIcon.getImageObserver());
 
-    private GraphicsConfiguration getDefaultConfiguration() {
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice gd = ge.getDefaultScreenDevice();
-        return gd.getDefaultConfiguration();
+        //drehen
+        AffineTransform affineTransform = AffineTransform.getRotateInstance(
+                Math.toRadians(degrees),
+                buImg.getWidth() / 2,
+                buImg.getHeight() / 2);
+        BufferedImage rotatedImage = new BufferedImage(buImg.getWidth(), buImg
+                .getHeight(), buImg.getType());
+        Graphics2D g = (Graphics2D) rotatedImage.getGraphics();
+        g.setTransform(affineTransform);
+        g.drawImage(buImg, 0, 0, null);
+
+        //to ImageIcon
+        ImageIcon imageI = new ImageIcon(rotatedImage);
+
+        return imageI;
     }
 
 }
