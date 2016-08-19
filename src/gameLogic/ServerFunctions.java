c210f33ab3f426604d7c0548a7a0e4d9f51770d8
Rehan1 <app.rehan@yahoo.de>
2016-08-20 01:10:12 +0200

    movePlayer Functions from GameFunctions zu ServerFunctions übernommen und leicht verändert

--- src/gameLogic/ServerFunctions.java
+++ src/gameLogic/ServerFunctions.java
@@ -25,6 +25,7 @@ public class ServerFunctions {
                         board.setTiles(1,i,board.getTile(1,i-1));
                     }
                     board.setTiles(1,0,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 1:
@@ -34,6 +35,7 @@ public class ServerFunctions {
                         board.setTiles(3,i,board.getTile(3,i-1));
                     }
                     board.setTiles(3,0,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 2:
@@ -43,6 +45,7 @@ public class ServerFunctions {
                         board.setTiles(5,i,board.getTile(5,i-1));
                     }
                     board.setTiles(5,0,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 3:
@@ -52,6 +55,7 @@ public class ServerFunctions {
                         board.setTiles(i,1,board.getTile(i+1,1));
                     }
                     board.setTiles(6,1,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 4:
@@ -61,6 +65,7 @@ public class ServerFunctions {
                         board.setTiles(i,3,board.getTile(i+1,3));
                     }
                     board.setTiles(6,3,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 5:
@@ -70,6 +75,7 @@ public class ServerFunctions {
                         board.setTiles(i,5,board.getTile(i+1,5));
                     }
                     board.setTiles(6,5,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 6:
@@ -79,6 +85,7 @@ public class ServerFunctions {
                         board.setTiles(5, i, board.getTile(5,i+1));
                     }
                     board.setTiles(5,6,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 7:
@@ -88,6 +95,7 @@ public class ServerFunctions {
                         board.setTiles(3, i, board.getTile(3,i+1));
                     }
                     board.setTiles(3,6,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 8:
@@ -97,6 +105,7 @@ public class ServerFunctions {
                         board.setTiles(1, i, board.getTile(1,i+1));
                     }
                     board.setTiles(1,6,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 9:
@@ -106,6 +115,7 @@ public class ServerFunctions {
                         board.setTiles(i,5,board.getTile(i-1,5));
                     }
                     board.setTiles(0,5,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 10:
@@ -115,6 +125,7 @@ public class ServerFunctions {
                         board.setTiles(i,3,board.getTile(i-1,3));
                     }
                     board.setTiles(0,3,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
 
                 case 11:
@@ -124,6 +135,7 @@ public class ServerFunctions {
                         board.setTiles(i,1,board.getTile(i-1,1));
                     }
                     board.setTiles(0,1,tmpTile);
+                    board = movePlayerIfStoneIsPlacedInMaze(board,  buttonID);
                     break;
                 default:
                     System.err.println("Button ID ist nicht Korrekt");
@@ -251,178 +263,7 @@ public class ServerFunctions {
     }
     //==================================================================================================================
 
-    /**
-     * placeNextStoneInMaze
-     * Function places a stone in the maze if it´s possible
-     * returns a whole board
-     * @param arrowButtonID
-     * @param boardFromClient
-     * @return boardFromClient
-     */
-    //board from client -> later from server
-    public void placeNextStoneInMaze(int arrowButtonID, Board boardFromClient, Tiles tmpStorageTile){
 
-        if(isArrowMoveAllowed(arrowButtonID)){
-            //if(true){
-            System.out.println("ffffffffff");
-            switch (arrowButtonID){
-                case 0:
-                    System.out.println("id=0");
-                    tmpStorageTile = boardFromClient.getTile(1, 6);
-                    //move all tiles one forward
-                    for (int index = 6; index > 0; index--) {
-                        boardFromClient.setTiles(1, index, boardFromClient.getTile(1, index - 1));
-                    }
-                    //place on the first spot the next tile
-                    boardFromClient.setTiles(1, 0, boardFromClient.getNextTile());
-                    //place the tmpStorageTile
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 1:
-                    tmpStorageTile = boardFromClient.getTile(3, 6);
-                    for (int index = 6; index > 0; index--) {
-                        boardFromClient.setTiles(3, index, boardFromClient.getTile(3, index - 1));
-                    }
-                    boardFromClient.setTiles(3, 0, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 2:
-                    tmpStorageTile = boardFromClient.getTile(5, 6);
-                    for (int index = 6; index > 0; index--) {
-                        boardFromClient.setTiles(5, index, boardFromClient.getTile(5, index - 1));
-                    }
-                    boardFromClient.setTiles(5, 0, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 8:
-                    tmpStorageTile = boardFromClient.getTile(1, 0);
-                    for (int index = 0; index < 6; index++) {
-                        boardFromClient.setTiles(1, index, boardFromClient.getTile(1, index + 1));
-                    }
-                    boardFromClient.setTiles(1, 6, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 7:
-                    tmpStorageTile = boardFromClient.getTile(3, 0);
-                    for (int index = 0; index < 6; index++) {
-                        boardFromClient.setTiles(3, index, boardFromClient.getTile(3, index + 1));
-                    }
-                    boardFromClient.setTiles(3, 6, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 6:
-                    tmpStorageTile = boardFromClient.getTile(5, 0);
-                    for (int index = 0; index < 6; index++) {
-                        boardFromClient.setTiles(5, index, boardFromClient.getTile(5, index + 1));
-                    }
-                    boardFromClient.setTiles(5, 6, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 11:
-                    tmpStorageTile = boardFromClient.getTile(6, 1);
-                    for (int index = 6; index > 0; index--) {
-                        boardFromClient.setTiles(index, 1, boardFromClient.getTile(index - 1, 1));
-                    }
-                    boardFromClient.setTiles(0, 1, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 10:
-                    tmpStorageTile = boardFromClient.getTile(6, 3);
-                    for (int index = 6; index > 0; index--) {
-                        boardFromClient.setTiles(index, 3, boardFromClient.getTile(index - 1, 3));
-                    }
-                    boardFromClient.setTiles(0, 3, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 9:
-
-                    tmpStorageTile = boardFromClient.getTile(6, 5);
-                    for (int index = 6; index > 0; index--) {
-                        boardFromClient.setTiles(index, 5, boardFromClient.getTile(index - 1, 5));
-                    }
-                    boardFromClient.setTiles(0, 5, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 3:
-                    tmpStorageTile = boardFromClient.getTile(0, 1);
-
-                    for (int index = 0; index < 6; index++) {
-                        boardFromClient.setTiles(index, 1, boardFromClient.getTile(index + 1, 1));
-                    }
-
-                    boardFromClient.setTiles(6, 1, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 4:
-                    tmpStorageTile = boardFromClient.getTile(0, 3);
-
-                    for (int index = 0; index < 6; index++) {
-                        boardFromClient.setTiles(index, 3, boardFromClient.getTile(index + 1, 3));
-                    }
-                    boardFromClient.setTiles(6, 3, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-
-                case 5:
-                    tmpStorageTile = boardFromClient.getTile(0, 5);
-                    for (int index = 0; index < 6; index++) {
-                        boardFromClient.setTiles(index, 5, boardFromClient.getTile(index + 1, 5));
-                    }
-                    boardFromClient.setTiles(6, 5, boardFromClient.getNextTile());
-                    boardFromClient.setNextTile(tmpStorageTile);
-
-                    //boardFromClient = movePlayerIfStoneIsPlacedInMaze(boardFromClient,  arrowButtonID);
-
-                    break;
-            }
-        }
-
-    }
-    //==================================================================================================================
 
 
     public void setRotation(int rot, Board board){
@@ -519,42 +360,90 @@ public class ServerFunctions {
         // Wenn kein Fall zutrift wird false zurückgegeben
         return false;
     }
-    //==================================================================================================================
 
 
-    /**
-     * IsPlayerGettingPoints
-     * Funktion, welche überprüft, ob der Spieler auf dem gesuchten Symbol steht. Falls ja wird ein Punkt dazu addiert
-     * return 0 = kein Punkt
-     * Return 1 = Punkt
-     * return 2 = spiel beenden
-     */
-    public int isPlayerGettingPoints(Board board, int playerID){
+    public Board movePlayerIfStoneIsPlacedInMaze(Board board, int arrowNumber){
+        int rownumber = 0;
+        switch (arrowNumber){
+            //top and bottom
+            case 0:
+            case 8:
+                rownumber = 1;
+                break;
+            case 1:
+            case 7:
+                rownumber = 3;
+                break;
+            case 2:
+            case 6:
+                rownumber = 5;
+                break;
+            //right and left
+            case 3:
+            case 11:
+                rownumber = 1;
+                break;
+            case 4:
+            case 10:
+                rownumber = 3;
+                break;
+            case 5:
+            case 9:
+                rownumber = 5;
+                break;
+        }
 
-        if(board.getPlayer(playerID).getCreaturesNeeded().get(0).getCreature() == board.getTile(board.getPlayer(playerID).getAcutalPosition().getX(),board.getPlayer(playerID).getAcutalPosition().getY()).getShape().getCreature()){
-            //falls sie identisch sind
-            // Punkt für den Spieler
+        for (int playerID = 0 ; playerID < board.getAllPlayers().length ; playerID ++) {
 
-            //erstes Element wird entfernt
-            board.getPlayer(playerID).getCreaturesNeeded().remove(0);
-
-            if(board.getPlayer(playerID).getCreaturesNeeded().isEmpty()){
-                //falls die Liste der NeededCreatures leer ist -> Spieler hat gewonnen
-                return 2;
+            // Für Pfeil 0 1 2 top
+            if (((arrowNumber==0) || (arrowNumber == 1) || (arrowNumber == 2)) && (board.getPlayer(playerID).getAcutalPosition().getX() == rownumber)) {
+                if (board.getPlayer(playerID).getAcutalPosition().getY() == 6) {
+                    // aus dem spielfeld schieben
+                    board.getPlayer(playerID).setActualPosition(new Position(rownumber, 0));
+                } else {
+                    //eins weiter schieben
+                    board.getPlayer(playerID).setActualPosition(new Position(rownumber, board.getPlayer(playerID).getAcutalPosition().getY() + 1));
+                }
             }
-            else{
-                System.out.println(board.getPlayer(playerID).getCreaturesNeeded().get(0).getCreature());
-                //Wert beim Spieler um 1 erhöhen
-                board.getPlayer(playerID).setScore(board.getPlayer(playerID).getScore() + 1);
-                return 1;
+            //----------------------------
+            // Für Pfeil  6 7 8 bottom
+            if (((arrowNumber == 6) || (arrowNumber == 7) || (arrowNumber == 8)) && (board.getPlayer(playerID).getAcutalPosition().getX() == rownumber)) {
+                if (board.getPlayer(playerID).getAcutalPosition().getY() == 0) {
+                    // aus dem spielfeld schieben
+                    board.getPlayer(playerID).setActualPosition(new Position(rownumber, 6));
+                } else {
+                    //eins weiter schieben
+                    board.getPlayer(playerID).setActualPosition(new Position(rownumber, board.getPlayer(playerID).getAcutalPosition().getY() - 1));
+                }
+            }
+            //----------------------------
+            // Für Pfeil  3 4 5 right
+            if (((arrowNumber == 3) || (arrowNumber == 4) || (arrowNumber == 5)) && (board.getPlayer(playerID).getAcutalPosition().getY() == rownumber)) {
+                if (board.getPlayer(playerID).getAcutalPosition().getX() == 0) {
+                    // aus dem spielfeld schieben
+                    board.getPlayer(playerID).setActualPosition(new Position(6,rownumber));
+                } else {
+                    //eins weiter schieben
+                    board.getPlayer(playerID).setActualPosition(new Position(board.getPlayer(playerID).getAcutalPosition().getX() - 1, rownumber));
+                }
+            }
+            //----------------------------
+            // Für Pfeil  9 10 11 right
+            if (((arrowNumber == 9) || (arrowNumber == 10) || (arrowNumber == 11)) && (board.getPlayer(playerID).getAcutalPosition().getY() == rownumber)) {
+                //setze Farbe zurück
+                if (board.getPlayer(playerID).getAcutalPosition().getX() == 6) {
+                    // aus dem spielfeld schieben
+                    board.getPlayer(playerID).setActualPosition(new Position(0,rownumber));
+                } else {
+                    //eins weiter schieben
+                    board.getPlayer(playerID).setActualPosition(new Position(board.getPlayer(playerID).getAcutalPosition().getX() + 1, rownumber));
+                }
             }
         }
-        else{
-            // kein Punkt erziehlt
-            return 0;
-        }
+        return board;
     }
     //==================================================================================================================
+
 
 
 }
